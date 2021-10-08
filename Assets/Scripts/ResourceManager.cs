5af53ba5d4b8311d77ea121d95ae80661c451aa8
catx <ivanshadow19@gmail.com>
2021-10-08 15:21:40 +0700

    Problem 8 done

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -27,9 +27,31 @@ public class ResourceManager : MonoBehaviour
             SpawnResource(_xRadius, _yRadius);
     }
 
+    public void SpawnResourceDelay()
+    {
+        Invoke("SpawnResource", 3f);
+    }
+
+    private void SpawnResource()
+    {
+        SpawnResource(_xRadius, _yRadius);
+    }
+
     private void SpawnResource(float x, float y)
     {
-        Instantiate(resourcePrefabs, RandomRadiusPosition(x, y), Quaternion.identity);
+        Debug.Log("...Trying to respawn");
+        Vector2 pos = RandomRadiusPosition(x, y);
+
+        // If somehow 'pos' inside player circle collider, repeat the function with same parameter
+        if(GameObject.FindGameObjectWithTag("Player").GetComponent<CircleCollider2D>().bounds.Contains(pos))
+        {
+            Debug.Log("...Respawn failed");
+            SpawnResource(x, y);
+            return;
+        }
+
+        Debug.Log("Respawned");
+        Instantiate(resourcePrefabs, pos, Quaternion.identity);
     }
 
     private Vector2 RandomRadiusPosition(float x, float y)
