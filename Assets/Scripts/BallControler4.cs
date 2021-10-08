d4a26de8d856ad949cc8e6798edb7664885e01ee
catx <ivanshadow19@gmail.com>
2021-10-08 13:54:31 +0700

    Problem 4 done

---
+++ Assets/Scripts/BallControler4.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControler4 : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Vector2 inputVector = Vector2.zero;
+
+    Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+        MoveCircle(inputVector, speed);
+    }
+
+    private void MoveCircle(Vector2 input, float velocity)
+    {
+        rb2d.velocity = inputVector * velocity;
+    }
+}
